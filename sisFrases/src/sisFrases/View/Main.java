5f7939ceefce68c6c2be9ece5d1381822d3540d0
rxpjunior <rxpjunior@gmail.com>
2021-06-24 17:08:13 -0300

    Acertando métodos no pacote DAO

--- sisFrases/src/sisFrases/View/Main.java
+++ sisFrases/src/sisFrases/View/Main.java
@@ -9,27 +9,30 @@ public class Main {
 		
 		//Inserir
 		Autor autor1 = new Autor();
-		autor1.setAutorNome("Jeremias");
+		autor1.setAutorNome("Juliana");
 		AutorDAO autorDao = new AutorDAO();
+		System.out.println("Validacao de insercao: "+autorDao.insere(autor1));
+		
+		autor1.setAutorNome("Juliano");
 		System.out.println("Validacao de insercao: "+autorDao.insere(autor1));
 		
 		//Buscar por ID
 		Autor autor2 = new Autor();
-		autor2 = autorDao.buscaPorId(9);
-		System.out.println("Retorno da busca pelo Id 9: "+autor2);
+		autor2 = autorDao.buscaPorId(2);
+		System.out.println("Retorno da busca pelo Id 2: "+autor2);
 		
 		
 		//Buscar todos os Autores
 		System.out.println("Retorno da busca por todos: "+autorDao.buscaTodos());
 		
 		//Altera autor
-		autor2.setAutorNome("Novo Nome");
+		autor2.setAutorNome("Maxwell");
 		autorDao.altera(autor2);
-		System.out.println("Autor do ID 9 aterado: "+autorDao.buscaPorId(9));
+		System.out.println("Autor do ID 2 aterado: "+autorDao.buscaPorId(2));
 		
 		//Deleta autor
-		autorDao.deleta(9);
-		System.out.println("Retorno da busca pelo ID 9 que acabou de ser excluido"+autorDao.buscaPorId(9));
+		System.out.println("Verificação deleção autor 1"+autorDao.deleta(1));
+		System.out.println("Retorno da busca pelo ID 1 que acabou de ser excluido"+autorDao.buscaPorId(1));
 		
 		
 
